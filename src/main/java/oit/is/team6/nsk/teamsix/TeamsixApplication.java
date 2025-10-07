04f69d2bd8e6eb21a351f1433587f74741953221
e1b23020 <e1b23020@oit.ac.jp>
2025-10-07 14:36:40 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team6/nsk/teamsix/TeamsixApplication.java
@@ -0,0 +1,13 @@
+package oit.is.team6.nsk.teamsix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsixApplication.class, args);
+	}
+
+}
